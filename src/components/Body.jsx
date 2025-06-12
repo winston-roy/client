1c7821ae0ce5cdcf6958a7b90822d607f7dfe4c9
winston.pashan <winston.pashan@99games.in>
2025-06-12 11:33:14 +0530

    redirect login page if not login & routing

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
 import NavBar from './NavBar'
 import { Outlet } from 'react-router-dom'
 import Footer from './Footer'
+import { BASE_URL } from '../utils/constants'
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux'
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
+
+
 
 function Body() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const res = await axios.get(BASE_URL + '/profile', { withCredentials: true });
+      dispatch(addUser(res.data['data']))
+    } catch (err) {
+      if (err.status === 401)
+        navigate("/login");
+      console.log('error---', err);
+    }
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
-      <div>
-          <NavBar />
-          <Outlet />
-          <Footer/>
+    <div>
+      <NavBar />
+      <Outlet />
+      <Footer />
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     const user = useSelector((store) => store.user);
@@ -7,7 +8,7 @@ const NavBar = () => {
     return (
         <div className="navbar bg-base-300 shadow-sm">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">DevTinder</a>
+                <Link to='/' className="btn btn-ghost text-xl">DevTinder</Link>
             </div>
             {user && (<div className="flex gap-2">
                 <div className='form-control mt-2'  >Welcome, {user.firstName}</div>
@@ -23,10 +24,10 @@ const NavBar = () => {
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                         <li>
-                            <a className="justify-between">
+                            <Link to='/profile' className="justify-between">
                                 Profile
                                 <span className="badge">New</span>
-                            </a>
+                            </Link>
                         </li>
                         <li><a>Settings</a></li>
                         <li><a>Logout</a></li>
