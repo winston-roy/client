47e57fd0ea985ee67a9f3176283359d49e928e78
winston.pashan <winston.pashan@99games.in>
2025-07-04 22:53:17 +0530

    socket connection fix

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -4,5 +4,5 @@ export const createSocketConnection = () => {
     if (location.hostname === "localhost")
         return io('http://localhost:7777');
     else
-        return io("/", {path:"/api/socket.io"})
+        return io("/", {path:"/api/v2/socket.io"})
 }
